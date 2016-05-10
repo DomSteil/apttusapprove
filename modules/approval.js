3d49327cd17df0a9196e909a50d8ff9fa1bde982
Dom Steil <dominicsteil@hotmail.com>
2016-05-10 16:35:28 -0700

    update approval

--- modules/approval.js
+++ modules/approval.js
@@ -1,36 +1,32 @@
 var nforce = require('nforce'),
     org = require('./auth').org,
 
-    APPROVAL_TOKEN = process.env.APPROVAL_TOKEN;
+    APPROVE_TOKEN = process.env.APPROVE_TOKEN;
 
 function execute(req, res) {
 
-    if (req.body.token != APPROVAL_TOKEN) {
+    if (req.body.token != APPROVE_TOKEN) {
         res.send("Invalid token");
         return;
     }
 
     var params = req.body.text.split(":");
-    var subject = params[0];
-    var description = params[1];
+    var iWa = params[0];
 
-    var a = nforce.createSObject('Approval');
-    a.set('Approval Status', status);
-    a.set('Comment', comment);
-
+    var c = nforce.updateSObject('Approval_Request');
+    c.set('iWa', iWa);
 
     org.insert({ sobject: c}, function(err, resp) {
         if (err) {
             console.error(err);
-            res.send("An error occurred while approving the quote");
+            res.send("An error occurred while approving the request");
         } else {
             var fields = [];
-            fields.push({title: "Approval Status", value: subject, short:false});
-            fields.push({title: "Comment", value: description, short:false});
-            fields.push({title: "Link", value: 'https://login.salesforce.com/' + resp.id, short:false});
+            fields.push({title: "iWa", value: iWa, short:false});
+ 
             var message = {
                 response_type: "in_channel",
-                text: "Quote has been approved:",
+                text: "Quote has been approved",
                 attachments: [
                     {color: "#009cdb", fields: fields}
                 ]
