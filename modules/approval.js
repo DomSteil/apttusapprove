196c43c67643f5c83c4c3d4893a74a5c4b9a3502
Dom Steil <dominicsteil@hotmail.com>
2016-05-11 10:37:49 -0700

    Push

--- modules/approval.js
+++ modules/approval.js
@@ -14,14 +14,28 @@ function execute(req, res) {
     var subject = params[0];
     var description = params[1];
 
-    var acc = nforce.createSObject('Account');
-    acc.Name = 'Spiffy Cleaners';
-    acc.Phone = '800-555-2345';
-    acc.SLA__c = 'Gold';
+    var c = nforce.createSObject('Case');
+    c.set('subject', subject);
+    c.set('description', description);
+    c.set('origin', 'Slack');
+    c.set('status', 'New');
 
-    org.insert(acc, oauth, function(err, resp){
-    if(!err) console.log('It worked!');
-
+    org.insert({ sobject: c}, function(err, resp) {
+        if (err) {
+            console.error(err);
+            res.send("An error occurred while creating a case");
+        } else {
+            var fields = [];
+            fields.push({title: "Subject", value: subject, short:false});
+            fields.push({title: "Description", value: description, short:false});
+            fields.push({title: "Link", value: 'https://login.salesforce.com/' + resp.id, short:false});
+            var message = {
+                response_type: "in_channel",
+                text: "A new case has been created:",
+                attachments: [
+                    {color: "#009cdb", fields: fields}
+                ]
+            };
             res.json(message);
         }
     });
