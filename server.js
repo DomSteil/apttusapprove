c885f3814ee928e51fd99df070badfec1f8427a2
Dom Steil <dominicsteil@hotmail.com>
2016-05-11 09:38:01 -0700

    again

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     auth = require('./modules/auth'),
     approval = require('./modules/approval'),
+    _case = require('./modules/case')
     app = express();
 
 app.set('port', process.env.PORT || 5000);
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/approval', approval.execute);
+app.post('/approve', approval.execute);
+app.post('/case', _case.execute);
 
 
 app.listen(app.get('port'), function () {
