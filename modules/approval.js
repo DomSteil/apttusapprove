8c0b76ed354d9ef033cb5497f43d5a79ddc08c46
Dom Steil <dominicsteil@hotmail.com>
2016-05-11 14:39:25 -0700

    update

--- modules/approval.js
+++ modules/approval.js
@@ -11,16 +11,12 @@ function execute(req, res) {
     }
 
     var params = req.body.text.split(":");
-    var subject = params[0];
-    var description = params[1];
+    var email= params[0];
+    var phone = params[1];
 
-    var c = nforce.createSObject('Case');
-    c.set('subject', subject);
-    c.set('description', description);
-    c.set('origin', 'Slack');
-    c.set('status', 'New');
-    c.set('type', 'Problem');
-    c.set('reason', 'Instructions not clear');
+    var c = nforce.createSObject('Contact');
+    c.set('email', email);
+    c.set('phone', phone);
 
 
     org.insert({ sobject: c}, function(err, resp) {
@@ -29,8 +25,8 @@ function execute(req, res) {
             res.send("An error occurred while creating a case");
         } else {
             var fields = [];
-            fields.push({title: "Subject", value: subject, short:false});
-            fields.push({title: "Description", value: description, short:false});
+            fields.push({title: "Email", value: email, short:false});
+            fields.push({title: "Phone", value: phone, short:false});
             fields.push({title: "Link", value: 'https://login.salesforce.com/' + resp.id, short:false});
             var message = {
                 response_type: "in_channel",
