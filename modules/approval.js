ced1534e93651d3f5596c08c2e09cc5810e3d555
Dom Steil <dominicsteil@hotmail.com>
2016-05-12 10:19:08 -0700

    morning push

--- modules/approval.js
+++ modules/approval.js
@@ -29,47 +29,4 @@ var querycase = 'SELECT id, Slack_Status__c, iWa__c FROM Apttus_Approval__Approv
     });
 
 
-
-
-
-
-
-
-
-
-
- /*   var params = req.body.text.split(":");
-    var subject = params[0];
-    var description = params[1];
-
-    var c = nforce.createSObject('Case');
-    c.set('subject', subject);
-    c.set('description', description);
-    c.set('origin', 'Slack');
-    c.set('status', 'New');
-    c.set('type', 'Question');
-    c.set('reason', 'Instructions not clear');
-
-
-    org.insert({ sobject: c}, function(err, resp) {
-        if (err) {
-            console.error(err);
-            res.send("An error occurred while creating a case");
-        } else {
-            var fields = [];
-            fields.push({title: "Quote:", value: subject, short:false});
-            fields.push({title: "Status:", value: description, short:false});
-            fields.push({title: "Link", value: 'https://login.salesforce.com/' + resp.id, short:false});
-            var message = {
-                response_type: "in_channel",
-                text: "Quote has been approved:",
-                attachments: [
-                    {color: "#009cdb", fields: fields}
-                ]
-            };
-            res.json(message);
-        }
-    });
-}
-
 exports.execute = execute;
