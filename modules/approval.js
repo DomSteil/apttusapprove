3d697dfb2bdca4c180bf7b737c0da5c1084d5157
Dom Steil <dominicsteil@hotmail.com>
2016-05-12 10:24:30 -0700

    morning push

--- modules/approval.js
+++ modules/approval.js
@@ -12,14 +12,14 @@ function execute(req, res) {
 
 
 
-var querycase = 'SELECT id, Slack_Status__c, iWa__c FROM Apttus_Approval__Approval_Request__c WHERE id = a1vj0000000seJN LIMIT 1';
+var querycase = 'SELECT Id, Slack_Status__c, iWa__c FROM Apttus_Approval__Approval_Request__c WHERE id = a1vj0000000seJN LIMIT 1';
     //500j000000CpvEDAAZ
     org.query({ query: querycase }, function(err, resp){
 
         if(!err && resp.records) {
 
             var c = resp.records[0];
-            c.set('iWa__c', 'TRUE');
+            c.set('iWa__c', TRUE);
             c.set('Slack_Status__c', 'Approved');
 
             org.update({ sobject: c, oauth: oauth }, function(err, resp){
